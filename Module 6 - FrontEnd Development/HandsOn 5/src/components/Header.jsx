84aa59d14e56dd5d1a8b1ced0392f41f0911de5d
Nityaa Suresh <nityaasuresh2005@gmail.com>
2026-07-05 21:52:10 +0530

    Create Header.jsx

---
+++ Module 6 - FrontEnd Development/HandsOn 5/src/components/Header.jsx
@@ -0,0 +1,33 @@
+/*
+Task 1: Project Setup & First Components
+
+Goal: Scaffold the React app and create the first reusable components.
+*/
+
+function Header({ siteName, enrolledCount }) {
+
+    return (
+
+        <header className="header">
+
+            <h1>{siteName}</h1>
+
+            <nav>
+
+                <a href="#">Home</a>
+
+                <a href="#">Courses</a>
+
+                <a href="#">Profile</a>
+
+            </nav>
+
+            <h3>Enrolled : {enrolledCount}</h3>
+
+        </header>
+
+    );
+
+}
+
+export default Header;
