0240cc5893a7cb24869739788cfab6b2eb86e1e8
Nityaa Suresh <nityaasuresh2005@gmail.com>
2026-07-05 21:52:38 +0530

    Create Footer.jsx

---
+++ Module 6 - FrontEnd Development/HandsOn 5/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+/*
+Task 1: Project Setup & First Components
+
+Goal: Scaffold the React app and create the first reusable components.
+*/
+
+function Footer(){
+
+    return(
+
+        <footer className="footer">
+
+            <p>&copy; 2026 Student Portal. All Rights Reserved.</p>
+
+        </footer>
+
+    );
+
+}
+
+export default Footer;
