32da3e036f70cd55114e2cf1537e6712ba72ace5
Nityaa Suresh <nityaasuresh2005@gmail.com>
2026-07-05 22:07:28 +0530

    Update main.jsx

--- Module 6 - FrontEnd Development/HandsOn 5/src/main.jsx
+++ Module 6 - FrontEnd Development/HandsOn 5/src/main.jsx
@@ -1,16 +1,9 @@
-/*
-Task 1: Project Setup & First Components
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 
-Goal: Scaffold the React app and create the first reusable components.
-*/
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./App.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
